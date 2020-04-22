c942059b38b7012d435589b35a5bf48761cc6111
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-22 18:41:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-    binding.pry
+
     { 
       railroads: {
         pieces: 4,
@@ -76,4 +76,5 @@ def monopoly_with_fourth_tier
         
       }
     }
+    binding.pry
 end
