e69ceabcf5b50519b6e4e79459f95fff6979313a
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-22 20:22:52 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
@@ -10,9 +12,11 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   { 
-    pieces: 4,
-    rent_in_dollars: {},
-    names: {}
+    railroads:{
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
+    }
   }
 end
 
@@ -22,18 +26,20 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   { 
-    pieces: 4,
-    rent_in_dollars: {
-      one_piece_owned: 25,
-      two_pieces_owned: 50,
-      three_pieces_owned: 100,
-      four_pieces_owned: 200
-    },
-    names: {
-      reading_railroad: {},
-      pennsylvania_railroad: {},
-      b_and_o_railroad: {},
-      shortline_railroad: {}
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+      },
+      names: {
+        reading_railroad: {},
+        pennsylvania_railroad: {},
+        b_and_o_railroad: {},
+        shortline_railroad: {}
+      }
     }
   }
 end
@@ -43,27 +49,32 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
     { 
-      pieces: 4,
-      rent_in_dollars: {
-        one_piece_owned: 25,
-        two_pieces_owned: 50,
-        three_pieces_owned: 100,
-        four_pieces_owned: 200
-      },
-      names: {
-        reading_railroad: {
-          mortage_value: 100
+      railroads: {
+        pieces: 4,
+        rent_in_dollars: {
+          one_piece_owned: 25,
+          two_pieces_owned: 50,
+          three_pieces_owned: 100,
+          four_pieces_owned: 200
         },
-        pennsylvania_railroad: {
-          mortage_value: 200
-        },
-        b_and_o_railroad: {
-          mortage_value: 400
-        },
-        shortline_railroad: {
-          mortage_value: 800
+        names: {
+          reading_railroad: {
+            mortgage_value: 100
+          },
+          pennsylvania_railroad: {
+            mortgage_value: 200
+          },
+          b_and_o_railroad: {
+            mortgage_value: 400
+          },
+          shortline_railroad: {
+            mortgage_value: 800
+          }
         }
+        
       }
     }
+
 end
