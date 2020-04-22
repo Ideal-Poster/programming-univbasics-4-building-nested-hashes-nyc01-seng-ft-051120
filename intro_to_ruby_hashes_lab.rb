641003d3f94cf1abbc7fcbe0630fa48029d5457d
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-22 18:38:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,6 +48,9 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
     { 
+      railroads: {
+        
+      }
       pieces: 4,
       rent_in_dollars: {
         one_piece_owned: 25,
