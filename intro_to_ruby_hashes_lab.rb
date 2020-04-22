1ef19ed21de16bca1be0c11001c04670d21a6ddd
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-22 18:40:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
@@ -47,6 +49,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+    binding.pry
     { 
       railroads: {
         pieces: 4,
